08b7f082371963677a199d5a3c4d8bdbfe87516c
Eduardo Marcos <edujugon@hotmail.com>
2016-12-15 12:32:27 +0100

    Improved index.php

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+
+if(!function_exists('config_path'))
+    require __DIR__.'/vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 //Database
-$database = function_exists('config_path') ? config_path('customDatabase.php') : require 'config/database.php';
-
+$database = function_exists('config_path') && file_exists(config_path('customDatabase.php')) ? require config_path('customDatabase.php') : require 'config/database.php';
 
 $databaseConfig = $database['connections'][$database['default']];
 
+$defaultDBdriver = (function_exists('env') && env('DB_CONNECTION')) ? env('DB_CONNECTION') : 'pgsql';
+
 //Get Laravel Project's config or use its own configuration.
 if(!$database['custom']){
-    $databaseConfig = function_exists('config_path') ? config_path('database.php')['connections'][function_exists('env') ? env('DB_CONNECTION') : config_path('database.php')['default']] : $databaseConfig;
+    $databaseConfig = function_exists('config_path') ? $database['connections'][$defaultDBdriver] : $databaseConfig;
 }
 
 
